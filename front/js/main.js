0824a0a0c43fce95b8779539666dd6617b94cc0a
KrishenK <fox2581kriskenk@gmail.com>
2022-10-31 21:09:42 +0100

    update home page

---
+++ front/js/main.js
@@ -0,0 +1,40 @@
+(function () {
+    var command = document.querySelector(".command");
+    [...document.querySelectorAll("* [data-command-text]")].forEach((el) => {
+        var i = 0;
+        var txt = el.dataset.commandText;
+        var speed = 50;
+
+        el.addEventListener("mouseenter", function () {
+            clearTimeout(command.dataset.timeoutId);
+            command.innerHTML = "";
+            typeWriter(el, txt, speed, i);
+        });
+
+        el.addEventListener("mouseleave", function () {
+            clearTimeout(command.dataset.timeoutId);
+            command.innerHTML = "";
+        });
+    });
+
+    function typeWriter(el, txt, speed, i) {
+        if (i < txt.length && command.innerHTML != txt) {
+            command.innerHTML += txt.charAt(i);
+            i++;
+            command.dataset.timeoutId = setTimeout(function () {
+                typeWriter(el, txt, speed, i);
+            }, speed);
+        }
+    }
+
+    var prevScroll = 0;
+
+    window.addEventListener('scroll', (e) => {
+        const deltaY = prevScroll - window.scrollY;
+        if (window.scrollY > 64) { // size of header
+            if (deltaY > 0) document.querySelector('header').classList.remove('hide');
+            else document.querySelector('header').classList.add('hide');
+        } else document.querySelector('header').classList.remove('hide');
+        prevScroll = window.scrollY;
+    })
+})();
