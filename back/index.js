984659fcb258f99194c1f9ac85355b6959c000c5
Atalata <kenny.lutz@laposte.net>
2022-11-17 23:21:53 +0100

    removed angular and added express 🤤

---
+++ back/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send("Hello world !");
+})
+
+app.listen(port, () => {
+    console.log("App running on http://localhost:" + port);
+})
