719b454a43ca105ff1c80586f4ee18a04e0f8229
Atalata <kenny.lutz@laposte.net>
2023-03-27 13:30:33 +0200

    small fix

--- pages/write-ups/index.js
+++ pages/write-ups/index.js
@@ -1,51 +1,46 @@
 import { getAllCategories, getAllPosts } from "../../helpers/md";
 
 function WriteUpsHome({ out }) {
-    return (
-        <>
-        <section>
-
-            <h2>Write-ups</h2>
-            <hr />
-                {out.map(({ category, posts }) => (
-                    <div className="accordion open">
-                        <div className="item">
-                            <button>
-                                <span>{category}</span>
-                                <i className="fa-solid fa-caret-right"></i>
-                            </button>
-                        </div>
-                        <div className="content">
-                            <div className="inner">
-                                {posts.map((post) => (
-                                        <article>
-                                            <div className="content">
-                                                <h3>
-                                                    {post.frontmatter.title}
-                                                </h3>
-                                                <small>{post.frontmatter.publishedDate}</small>
-                                                <p>
-                                                    {post.frontmatter.description}
-                                                </p>
-                                            </div>
-                                        <div className="tags">
-                                            <ul>
-                                                {post.frontmatter.tags.map((tag) => (
-                                                <li key={tag}>
-                                                    <a href="#">{tag}</a>
-                                                </li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                    </article>
-                                ))}
-                            </div>
-                        </div>
+  return (
+    <>
+      <section>
+        <h2>Write-ups</h2>
+        <hr />
+        {out.map(({ category, posts }) => (
+          <div key={category} className="accordion open">
+            <div className="item">
+              <button>
+                <span>{category}</span>
+                <i className="fa-solid fa-caret-right"></i>
+              </button>
+            </div>
+            <div className="content">
+              <div className="inner">
+                {posts.map((post) => (
+                  <article key={post.slug}>
+                    <div className="content">
+                      <h3>{post.frontmatter.title}</h3>
+                      <small>{post.frontmatter.publishedDate}</small>
+                      <p>{post.frontmatter.description}</p>
                     </div>
+                    <div className="tags">
+                      <ul>
+                        {post.frontmatter.tags.map((tag) => (
+                          <li key={tag}>
+                            <a href="#">{tag}</a>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </article>
                 ))}
-        </section>
-        </>
-    )
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+    </>
+  );
 }
 
 export const getStaticProps = async () => {
