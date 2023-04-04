1be7fab7e5ac07059696d13b804243c250280374
KrishenK <fox2581kriskenk@gmail.com>
2023-04-04 22:30:12 +0200

    added tyranic project master ideas

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "max-width": "100%",
+            img: {
+              "margin-left": "auto",
+              "margin-right": "auto",
+              "display": "block"
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
